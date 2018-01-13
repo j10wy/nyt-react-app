aae9f9a7005971c877c02524e23b2ed4f3fd62bc
Jeffrey Lowy <jeffreylowy@gmail.com>
2018-01-12 16:10:58 -0800

    Basic structure for app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
+
+// Serve up static assets
+app.use(express.static("nytreact/build"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Use apiRoutes
+app.use("/api", apiRoutes);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(`${__dirname}/nytreact/build/index.html`);
+});
+
+app.listen(PORT, function() {
+  console.log(`Server now on port http://localhost:${PORT}!`);
+});
